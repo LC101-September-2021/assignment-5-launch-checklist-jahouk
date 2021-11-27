43b9d38786e89bae11e8168ec3916161a776ad15
jahouk <jahouk@houkstead.com>
2021-11-26 19:38:10 -0600

    Stashing via commit

--- script.js
+++ script.js
@@ -1,16 +1,61 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+// const {validateInput} = require("./scriptHelper");
+// const { addDestinationInfo, validateInput, formSubmission, pickPlanet, myFetch } = require("./scriptHelper");
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+
+const valdatedResults = {
+  empty: "Empty",
+  nan: "Not a Number",
+  number: "Is a Number",
+};
+
+window.addEventListener("load", function () {
+    console.log("Loaded");
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function (event) {
+    // event.preventDefault();
+    console.log("Submit Clicked");
+    let readyForLaunch = true;
+    let pilotName = document.querySelector("input[name=pilotName]").value;
+    let copilotName = document.querySelector("input[name=copilotName]").value;
+    let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+    let cargoMass = document.querySelector("input[name=cargoMass]").value;
+
+    if (
+      validateInput(pilotName) != valdatedResults.nan ||
+      validateInput(copilotName) != valdatedResults.nan ||
+      validateInput(fuelLevel) != valdatedResults.number ||
+      validateInput(cargoMass) != valdatedResults.number
+    ) {
+      readyForLaunch = false;
+    }
+
+    console.log(
+      "readyForLaunch: " +
+        readyForLaunch +
+        "\npilotName: " +
+        pilotName +
+        "\ncopilotName: " +
+        copilotName +
+        "fuelLevel: " +
+        fuelLevel +
+        "cargoMass: " +
+        cargoMass
+    );
+    
+  });
+
+  let listedPlanets;
+  // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+  let listedPlanetsResponse;
+  listedPlanetsResponse
+    .then(function (result) {
+      listedPlanets = result;
+      console.log(listedPlanets);
+    })
+    .then(function () {
+      console.log(listedPlanets);
+      // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    });
 });
